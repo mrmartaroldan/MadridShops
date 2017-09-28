a488bfb8f9e11b0c0c2870bd2bee0b3d4e64c701
Marta Roldan <mrmartaroldan@gmail.com>
2017-09-28 08:32:24 +0100

    Upload activities from internet. Using NSOperationQueue

---
+++ MadridShops/Model/Interactors/CacheInteractors/ActivitiesInteractor/DownloadAllActivitiesInteractorNSOpImpl.swift
@@ -0,0 +1,9 @@
+//
+//  DownloadAllActivitiesInteractorNSOpImpl.swift
+//  MadridShops
+//
+//  Created by Marta Roldán Arellano on 27/9/17.
+//  Copyright © 2017 Marta Roldán Arellano. All rights reserved.
+//
+
+import Foundation
